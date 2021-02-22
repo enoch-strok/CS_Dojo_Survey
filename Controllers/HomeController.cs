00871511faf68aead4d2960a005492416b2bf497
enoch-strok <enochstrok@outlook.com>
2021-02-22 07:40:01 -0800

    Completed Dojo Survey

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CS_Time_Display_B.Models;
+
+namespace CS_Time_Display_B.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("method")]
+        public IActionResult Method()
+        {
+            // Do something with form input
+            return View();
+        }
+
+        [HttpPost]
+        [Route("Results")]
+        public IActionResult Results(string Name, string Location, string Language, string Comment)
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(Location);
+            Console.WriteLine(Language);
+            Console.WriteLine(Comment);
+
+            ViewBag.Name = Name;
+            ViewBag.Location = Location;
+            ViewBag.Language = Language;
+            ViewBag.Comment = Comment;
+            // Do something with form input
+            return View();
+            
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
